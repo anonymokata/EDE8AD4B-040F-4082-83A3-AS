789a16156cac0c7196f9a4610e7947235d2c09a7
anonymous <anonymous>
2016-08-02 17:16:02 -0400

    Rename test to be more accurate

--- calculator_test.c
+++ calculator_test.c
@@ -4,9 +4,15 @@
 #include <check.h>
 #include "calculator.h"
 
-START_TEST(when_is_valid_roman_numeral_is_passed_a_value_correct_response_returned)
+START_TEST(when_is_valid_roman_numeral_is_passed_one_digit_correct_response_returned)
 {
     ck_assert_int_eq(is_valid_roman_numeral("I"), true);
+    ck_assert_int_eq(is_valid_roman_numeral("V"), true);
+    ck_assert_int_eq(is_valid_roman_numeral("X"), true);
+    ck_assert_int_eq(is_valid_roman_numeral("L"), true);
+    ck_assert_int_eq(is_valid_roman_numeral("C"), true);
+    ck_assert_int_eq(is_valid_roman_numeral("D"), true);
+    ck_assert_int_eq(is_valid_roman_numeral("M"), true);
 }
 END_TEST
 
@@ -15,7 +21,7 @@ Suite *roman_numeral_calculator_suite(void)
     Suite *test_suite = suite_create("Roman Numeral Calculator");
     TCase *tc_core = tcase_create("Core");
 
-    tcase_add_test(tc_core, when_is_valid_roman_numeral_is_passed_a_value_correct_response_returned);
+    tcase_add_test(tc_core, when_is_valid_roman_numeral_is_passed_one_digit_correct_response_returned);
     suite_add_tcase(test_suite, tc_core);
 
     return test_suite;

72bab38165c4ecc051f6ada68723b075b5eceb45
anonymous <anonymous>
2016-08-02 17:53:11 -0400

    naive implementation roman_numeral_to_int

--- calculator.c
+++ calculator.c
@@ -13,6 +13,34 @@ bool is_valid_roman_numeral(char str[]) {
 int roman_numeral_to_int(char str[]) {
     if(is_valid_roman_numeral(str) == false)
         return -1;
-        
-    return 0;
+    int value = 0;
+        for(int i = 0; i < strlen(str); ++i) {
+            switch(str[i]) {
+                case 'I':
+                    value += 1;
+                    break;
+                case 'V':
+                    value += 5;
+                    break;
+                case 'X':
+                    value += 10;
+                    break;
+                case 'L':
+                    value += 10;
+                    break;
+                case 'C':
+                    value += 10;
+                    break;  
+                case 'D':
+                    value += 10;
+                    break;
+                case 'M':
+                    value += 10;
+                    break;
+                default:
+                //this should never actually happen due to the earlier check. Consider refactoring later
+                    return -1;                                                                                        
+            }
+        }    
+    return value;
 }
